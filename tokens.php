2eaa9fda1a4aa7d34887cb638434f72f7ec1fcff
Adetunjieliazer <adetunjieliazer@gmail.com>
2023-10-17 20:34:26 +0100

    Random-tokens-using-php

--- tokens.php
+++ tokens.php
@@ -1,9 +1,14 @@
-$randomIntegers = [];
+<?php 
+
+$randomIntegers;
 
 for ($i = 0; $i < 4; $i++) {
-    $randomIntegers[] = rand(1000, 9999) . "-" . rand(1000, 9999) . "-" . rand(1000, 9999) . "-" . rand(1000, 9999) . "-" . rand(1000, 9999);
+    $randomIntegers = rand(1000, 9999) . "-" . rand(1000, 9999) . "-" . rand(1000, 9999) . "-" . rand(1000, 9999) . "-" . rand(1000, 9999);
 }
 
 echo $randomIntegers;
+echo "\n";
 
 // $randomIntegers now contains an array of 4 random 4-digit integers
+
+?>
